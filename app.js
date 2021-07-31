f167ecd9f6b29af1708cc7f4225b9541c66cbe2b
Zach Stone <zachinjapan888s@gmail.com>
2021-07-31 11:44:18 +0900

    fix easter egg 2 js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const button = document.getElementById('joke-button');
 
+// event listner 
+button.addEventListener('click', getJoke);
+
 const jokeText = document.querySelector("h3");
+
+// joke variable to use in the if statment to determine if it is a two part joke
+let joke = "";
 
 // get joke from joke API
 async function getJoke() {
 
-    // joke variable to use in the if statment to determine if it is a two part joke
-    let joke = "";
 
     const apiUrl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
 
@@ -31,9 +35,6 @@ async function getJoke() {
     }
 };
 
-// event listner 
-button.addEventListener('click', getJoke);
-
 
 // music easter egg
 
@@ -41,15 +42,21 @@ const robot = document.getElementById("robot");
 
 robot.addEventListener('click', backgroundMusic);
 
-let robotClickCount = 0;
+let robotClickCount = 1;
 
 let music = new Audio("./audio/8bit.mp3");
 
 function backgroundMusic() {
-    if (robotClickCount >= 20) {
+    if (robotClickCount >= 20 && robotClickCount % 2 == 0) {
         music.play();
-    } else {
         robotClickCount++;
+    } else {
+        if (robotClickCount >= 20 && (robotClickCount % 2 == 1)) {
+            music.pause();
+            robotClickCount++;
+        } else {
+            robotClickCount++;
+        }
     }
 };
 
